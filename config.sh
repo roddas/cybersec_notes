f3150a09e072b15e0ce701c027ddd4555bfd9105
roddascabral <roddas360@gmail.com>
2024-07-25 12:51:42 -0400

    Add the Kali LInux configuration script

---
+++ config.sh
@@ -0,0 +1,12 @@
+
+sudo apt update 
+git clone https://github.com/swisskyrepo/PayloadsAllTheThings/
+git clone https://github.com/s0md3v/XSStrike
+
+sudo apt install seclists -y
+
+
+
+## REferences
+
+# https://swisskyrepo.github.io/PayloadsAllTheThings/#documentation
